8ef6f63685383a7c4a32c24324dfb0502a24d982
Tobias Kupek <mail@tkupek.eu>
2020-10-03 17:27:09 +0200

    try to call model

--- train.py
+++ train.py
@@ -6,7 +6,11 @@ from __future__ import division
 
 import argparse
 
+import torch
+
 from models.model_builder import Summarizer
+
+from utils import preprocess_data
 
 model_flags = ['hidden_size', 'ff_size', 'heads', 'inter_layers', 'encoder', 'ff_actv', 'use_interval', 'rnn_size']
 
@@ -16,7 +20,11 @@ def train(args):
 
     model = Summarizer(args, device, load_pretrained_bert=True)
     print(model)
-    model(['Hello', 'everyone', '.', 'Welcome', '.'])
+
+    train_data, train_results, test_data, test_results = preprocess_data.get_data()
+
+    ids = torch.tensor(train_data[0].ids)
+    model(ids)  # TODO
 
 
 if __name__ == '__main__':
@@ -25,4 +33,4 @@ if __name__ == '__main__':
     parser.add_argument("-encoder", default='classifier')
     parser.add_argument("-param_init", default=0.0)
     args = parser.parse_args()
-    # train(args)
+    train(args)
